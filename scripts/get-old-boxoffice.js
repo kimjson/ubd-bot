e28f6e3a711e36fdedd50371a8a398aa2a9747c3
Stein Kim <wotjd1102@gmail.com>
2019-05-15 01:52:00 +0900

    과거 영화 흥행 성적 수집 스크립트 일단위로 변경

--- scripts/get-old-boxoffice.js
+++ scripts/get-old-boxoffice.js
@@ -9,19 +9,15 @@ const main = async () => {
   const to = moment(args[1]);
 
   const range = moment.range(from, to);
-  const weeks = Array.from(range.by('week')).reverse();
+  const days = Array.from(range.by('day')).reverse();
 
   const movieService = await MovieService.build();
 
-  for (let week of weeks) {
-    const response = await koficService.getWeeklyBoxOffice(week);
-    const { showRange, weeklyBoxOfficeList } = response.boxOfficeResult;
+  for (let day of days) {
+    const boxOfficeList = await koficService.findDailyBoxOfficeListByDate(day);
+    const countedAt = day.toDate();
 
-    const tokens = showRange.split('~');
-    const endOfWeek = moment(tokens[1]);
-    const countedAt = endOfWeek.toDate();
-
-    for (let boxOffice of weeklyBoxOfficeList) {
+    for (let boxOffice of boxOfficeList) {
       const { movieNm: title, audiAcc: audiences } = boxOffice;
       const movie = { title, audiences: Number(audiences), countedAt }
 
